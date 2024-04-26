7ac3c8eaa657f7acb3bc43c68eb0d4ad89550624
GabeXxX <bruni.gabriele@outlook.com>
2024-04-26 15:16:29 +0200

    added  Matrix class dec

--- Challenge-2/Matrix.hpp
+++ Challenge-2/Matrix.hpp
@@ -1,4 +1,19 @@
 
 namespace algebra{
 
+    enum class StorageOrder{
+        RowMajor,
+        ColumnMajor
+    };
+
+    template<typename T, StorageOrder Order>
+    class Matrix
+    {
+    private:
+        /* data */
+    public:
+        Matrix(/* args */){};
+    };
+    
+
 }
