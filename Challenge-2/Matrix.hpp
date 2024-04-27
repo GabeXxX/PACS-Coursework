bcd502bb4dcbdaad19268c7260836ba06f2b98d7
GabeXxX <bruni.gabriele@outlook.com>
2024-04-27 16:10:36 +0200

    add constructor

--- Challenge-2/Matrix.hpp
+++ Challenge-2/Matrix.hpp
@@ -1,18 +1,35 @@
+#include <map>
+#include <array>
 
 namespace algebra{
 
+    /*!
+    * Specify the matrix ordering
+    */
     enum class StorageOrder{
-        RowMajor,
-        ColumnMajor
+        ROWMAJOR,
+        COLMAJOR
     };
 
+    /*!
+    *   @tparam T tyep of the element in the matrix
+    *   @tparam StorageOrder storage order of the matrix
+    */
     template<typename T, StorageOrder Order>
-    class Matrix
-    {
-    private:
-        /* data */
-    public:
-        Matrix(/* args */){};
+    class Matrix{
+
+        private:
+            std::map<std::array<std::size_t, 2>, T> data;
+            std::size_t nRows = 0;
+            std::size_t nColumns = 0;
+
+        public:
+
+            /*Constructor that takes the size of the matrix
+            * @param nRows Number of rows
+            * @param nColumns Number of columns
+            */
+            Matrix(std::size_t nRows, std::size_t nColumns) : nRows{nRows}, nColumns{nColumns} {};
     };
     
 
--- Challenge-2/main.cpp
+++ Challenge-2/main.cpp
@@ -1,8 +1,10 @@
 #include "Matrix.hpp"
+#include <iostream>
+
 
 using namespace algebra;
 
 int main()
 {
-
+    std::cout<<"Hello world";
 }
