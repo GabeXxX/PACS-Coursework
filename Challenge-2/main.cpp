c2a02ada3d7fbff74ddbf2b5a0788e704a134c43
GabeXxX <bruni.gabriele@outlook.com>
2024-05-02 17:24:48 +0200

    update main

--- Challenge-2/main.cpp
+++ Challenge-2/main.cpp
@@ -8,24 +8,7 @@ int main()
 {
 
     Matrix<double, StorageOrder::ROWMAJOR> test_matrix(4, 3);
-    //std::cout<< (test_matrix(1,1) = 2)<<std::endl;
-    //test_matrix.print();
-    //test_matrix.compress(); //ok 
-    //test_matrix(1,1) = 2; //ok
-    // test_matrix.compress(); // ok
- 
-    // test_matrix(1,1) = 2; //ok
-    // test_matrix.compress();
-    //test_matrix.print(); // ok
 
-    //test_matrix.uncompress(); //ok
-    std::string file_path = "./assets/lnsp_131.mtx";
-    test_matrix.read_from_file(file_path);  // ok
-    //test_matrix.print();
-    //test_matrix.compress();
-    //test_matrix.print();
-
-    //test vector multiplication 
     std::vector<double> unary_vector(131, 1);
 
     std::vector<double> result = test_matrix.operator*(unary_vector);
